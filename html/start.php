b35de47eca023812d2f7c4eab5a40c111a3a43b7
Emika2710 <164334957+Emika2710@users.noreply.github.com>
2024-12-05 15:18:03 +0100

    mitschreiben

--- html/start.php
+++ html/start.php
@@ -1,12 +1,14 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
 
-spl_autoload_register(function($class) {
-    include str_replace('\\', '/', $class) . '.php';
-});
+    spl_autoload_register(function($class) {
+        include str_replace('\\', '/', $class) . '.php';
+    });
 
-session_start();
+    $service = new Utils\BackendService("https://online-lectures-cs.thi.de/chat/", "f00a3c26-3aa4-40c6-a772-5adebc4c3689");
+    session_start();
+
 
 ?>
