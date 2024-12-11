1748f07f97a75a85486648e8cc2edd0922613e71
m-pencilcase <mel2575@thi.de>
2024-12-11 11:35:17 +0100

    Veränderungen in der User File, ich hab noch keinen Plan, ob es richtig läuft

--- html/Model/user.php
+++ html/Model/user.php
@@ -1,5 +1,22 @@
 <?php
- namespace	Model;
- class	User	{
- }
- ?>
+namespace Model;
+use JsonSerializable;
+
+class User implements JsonSerializable{
+    private $username;
+    //Implementation der Funktion für JsonSerializable
+    public	function	jsonSerialize():mixed	{
+        return	get_object_vars($this);
+        }
+
+    // konstruktor
+    public function __constructor($username = null) {
+        $this->username = $username;
+    }
+
+    // Getter-Funktion für das Attribut username
+    public function getUsername() {
+        return $this->username;
+    }
+}
+?>
