151a234541d29655c6c135ce9a60c36df4bb45da
m-pencilcase <mel2575@thi.de>
2024-12-12 14:51:54 +0100

    Übung 4 anfang

---
+++ Übungen/PHP_Übungen/Formulardatenverarbeitung.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Formulardatenverarbeitung</title>
+    </head>
+    <body>
+        <form method="get" action="test.php">
+            <input name="test">
+            <button type="submit">Absenden</button>
+    </body>
+</html>
