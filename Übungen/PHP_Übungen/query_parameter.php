d56365176079d24e0895e28c68ff48d46711e76c
m-pencilcase <mel2575@thi.de>
2024-12-12 13:46:42 +0100

    Übung 1 Gemacht

--- Übungen/PHP_Übungen/query_parameter.php
+++ Übungen/PHP_Übungen/query_parameter.php
@@ -1,13 +1,27 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>Query Parameter Test</title>
+    <title>Query Parameter Test</title>
 </head>
+
 <body>
-    <!--	...	-->
+    <!--erster Test, ob überhaupt ein Wert angegeben wird, wenn man test=1234 anfügt-->
+    <?php 
+    // echo	$_GET["test"];
+    ?>
+    
     <?php
-    echo	$_GET["test"];
-        //	...
+    if (isset($_GET["test"])) {
+        if (!empty($_GET["test"])) {
+            echo "Wert:	" . $_GET["test"];
+        } else {
+            echo "Kein	Wert!";
+        }
+    } else {
+        echo "Kein	Parameter	übergeben";
+    }
     ?>
 </body>
+
 </html>
