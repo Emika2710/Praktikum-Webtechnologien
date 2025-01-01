e29764afc97a093e1b37287312c6529ffd3bdf16
Eva Winter <emw8900@thi.de>
2025-01-01 18:45:53 +0100

    Aufgabe a

--- html/login.php
+++ html/login.php
@@ -5,6 +5,12 @@
     <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8" />
     <title>Login</title>
+
+    
+    <!-- Bootstrap CSS -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.rtl.min.css" integrity="sha384-+qdLaIRZfNu4cVPK/PxJJEy0B0f3Ugv8i482AKY7gwXwhaCroABd086ybrVKTa0q" crossorigin="anonymous">
+    
 </head>
 
 <body>
@@ -60,6 +66,10 @@
         </div>
 
     </form>
+
+    <!-- JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>    
+
 </body>
 
 </html>
--- html/register.php
+++ html/register.php
@@ -90,6 +90,11 @@ if (isset($_POST["action"]) && $_POST["action"] == "register") {
     <title>Register</title>
     <link rel="stylesheet" href="style.css">
 
+    <!-- Bootstrap CSS -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.rtl.min.css" integrity="sha384-+qdLaIRZfNu4cVPK/PxJJEy0B0f3Ugv8i482AKY7gwXwhaCroABd086ybrVKTa0q" crossorigin="anonymous">
+    
+
 </head>
 
 <body>
@@ -115,6 +120,9 @@ if (isset($_POST["action"]) && $_POST["action"] == "register") {
         </div>
         
     </form>
+
+    <!-- JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>    
 
 </body>
 
